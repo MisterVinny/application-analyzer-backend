35791137130ff6d1e36708a91b12338f34cb6e27
vinnyk <ragdolleye@gmail.com>
2021-06-22 14:04:12 -0500

    Finishes backend user routes. A user can be created by anyone, actions for show, update and destroy require authentication and have been tested to only operate for a current_user and their respective ID. Starts applications controller and restful routes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,59 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user, except: [:create]
+
   def create
-    user = User.new(
+    user = User.new({
       username: params[:username],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
-    )
+    })
     if user.save
       render json: { message: "User created successfully" }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def show
+    if params[:id].to_i == current_user.id.to_i
+      user = User.find(params[:id])
+      render json: user.as_json
+    else
+      render json: { errors: "Unauthorized to view content." }, status: :unauthorized
+    end
+    
+  end
+  
+  def update
+    if params[:id].to_i == current_user.id.to_i
+      user = User.find(params[:id])
+      user.username = params[:username] || user.username
+      user.address = params[:address] || user.address
+      user.email = params[:email] || user.email
+  
+      if user.save
+        render json: { message: "User updated successfully" }, status: :ok
+      else
+        render json: { errors: user.errors.full_messages }, status: :bad_request
+      end
+
+    else
+      render json: { errors: "Unauthorized to edit content." }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    if params[:id].to_i == current_user.id.to_i
+      user = User.find(params[:id])
+      user.destroy
+      render json: { message: "User has been successfully deleted." }
+    else
+      render json: { errors: "Unauthorized to delete user." }, status: :unauthorized
+    end
+
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  # User routes
   post "/users" => "users#create"
+  get "/users/:id" => "users#show"
+  patch "users/:id" => "users#update"
+  delete "users/:id" => "users#destroy"
+  
   post "/sessions" => "sessions#create"
+
+  # Application routes
+  get "/applications" => "applications#index"
+  
 end
