3a91f5589e16c97cbeb145bc936b3d21783c2513
vinnyk <ragdolleye@gmail.com>
2021-07-01 12:43:42 -0500

    Adds validation for address in user creation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :applications
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true
+  validates :address, presence: true
 end
